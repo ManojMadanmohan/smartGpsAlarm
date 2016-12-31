0b1a5ad5eee31834667722d7531398d296f7546b
Manoj <manoj@zophop.com>
2016-12-31 17:58:21 +0530

    from n to json for geofence request

--- app/src/main/java/alarm/manoj/com/smartgpsalarm/models/DefaultGeoFenceRequest.java
+++ app/src/main/java/alarm/manoj/com/smartgpsalarm/models/DefaultGeoFenceRequest.java
@@ -1,6 +1,8 @@
 package alarm.manoj.com.smartgpsalarm.models;
 
 import com.google.android.gms.maps.model.LatLng;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class DefaultGeoFenceRequest
 {
@@ -8,6 +10,10 @@ public class DefaultGeoFenceRequest
     private int _radiusMeters;
 
     private static final int LOCATION_MULTIPLIER = 1000;
+
+    private static final String KEY_RADIUS = "radius";
+    private static final String KEY_LAT = "lat";
+    private static final String KEY_LONG = "lon";
 
     public DefaultGeoFenceRequest(LatLng latLng, int radius)
     {
@@ -34,5 +40,22 @@ public class DefaultGeoFenceRequest
     public int hashCode()
     {
         return (int)Math.round(_latLng.latitude*LOCATION_MULTIPLIER + _latLng.longitude*LOCATION_MULTIPLIER + _radiusMeters);
+    }
+
+    public static String toJson(DefaultGeoFenceRequest defaultGeoFenceRequest) throws JSONException
+    {
+        JSONObject object = new JSONObject();
+        object.put(KEY_RADIUS, defaultGeoFenceRequest.getRadiusMeters());
+        object.put(KEY_LAT, defaultGeoFenceRequest.getLatLng().latitude);
+        object.put(KEY_LONG, defaultGeoFenceRequest.getLatLng().longitude);
+        return object.toString();
+    }
+
+    public static DefaultGeoFenceRequest fromJson(String json) throws JSONException
+    {
+        JSONObject jsonObject = new JSONObject(json);
+        LatLng latLng = new LatLng(jsonObject.getDouble(KEY_LAT), jsonObject.getDouble(KEY_LONG));
+        int radius = jsonObject.getInt(KEY_RADIUS);
+        return new DefaultGeoFenceRequest(latLng, radius);
     }
 }
