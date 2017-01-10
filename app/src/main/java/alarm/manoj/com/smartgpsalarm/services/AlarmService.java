0be655b0e4782363f718398b06e4c9cc6924caf6
Manoj <manoj@zophop.com>
2017-01-10 23:08:08 +0530

    alarm service implementation

--- app/src/main/java/alarm/manoj/com/smartgpsalarm/services/AlarmService.java
+++ app/src/main/java/alarm/manoj/com/smartgpsalarm/services/AlarmService.java
@@ -1,13 +1,30 @@
 package alarm.manoj.com.smartgpsalarm.services;
 
+import alarm.manoj.com.smartgpsalarm.features.AlarmFeature;
 import android.app.IntentService;
+import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.util.Log;
 import android.widget.Toast;
 import com.google.android.gms.location.GeofencingEvent;
 
+import java.io.IOException;
+
 public class AlarmService extends IntentService
 {
+    public static final String KEY_ALARM = "alarm_id";
+
+    public static Intent getLaunchIntent(Context context, String alarmId)
+    {
+        Intent intent = new Intent(context, AlarmService.class);
+        intent.putExtra(KEY_ALARM, alarmId);
+        return intent;
+    }
+
     public AlarmService()
     {
         this("Default");
@@ -23,16 +40,44 @@ public class AlarmService extends IntentService
     {
         //Runs in background thread
         GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
-        String val = intent.getStringExtra("dummy");
-        Log.d("test",val);
-        Toast.makeText(this, val, Toast.LENGTH_SHORT).show();
-        if(geofencingEvent.hasError())
+        String alarmId = intent.getStringExtra(KEY_ALARM);
+        triggerAlarm();
+        AlarmFeature.getInstance(this).unsetAlarm(alarmId);
+    }
+
+    private void triggerAlarm()
+    {
+        Toast.makeText(this, "GEOFENCE FOUND!!", Toast.LENGTH_LONG).show();
+        Uri alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        if(alert == null)
         {
-            Toast.makeText(this, "geofence error", Toast.LENGTH_SHORT).show();
-        } else
+            // alert is null, using backup
+            alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+            // I can't see this ever being null (as always have a default notification)
+            // but just incase
+            if(alert == null)
+            {
+                // alert backup is null, using 2nd backup
+                alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
+            }
+        }
+        try
         {
-            Toast.makeText(this, "got geofence alarm! Yay :)", Toast.LENGTH_SHORT).show();
-            //EventBus.getDefault().post(geofencingEvent);
+            MediaPlayer player = new MediaPlayer();
+            player.setDataSource(this, alert);
+            final AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
+            if (audioManager.getStreamVolume(AudioManager.STREAM_ALARM) != 0)
+            {
+                player.setAudioStreamType(AudioManager.STREAM_ALARM);
+                player.setLooping(false);
+                player.prepare();
+                player.start();
+            }
+        }catch (IOException ioex)
+        {
+            Toast.makeText(this, "IO EXP",Toast.LENGTH_LONG).show();
         }
     }
 }
