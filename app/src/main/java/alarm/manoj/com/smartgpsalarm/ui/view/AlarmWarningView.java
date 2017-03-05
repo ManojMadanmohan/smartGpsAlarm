c415762e1ed9dd82de491eb3edbd1a622c18f155
Manoj <manoj@zophop.com>
2017-03-06 00:31:03 +0530

    alarm warning given its own java view

---
+++ app/src/main/java/alarm/manoj/com/smartgpsalarm/ui/view/AlarmWarningView.java
@@ -0,0 +1,31 @@
+package alarm.manoj.com.smartgpsalarm.ui.view;
+
+import alarm.manoj.com.smartgpsalarm.R;
+import alarm.manoj.com.smartgpsalarm.features.AlarmRinger;
+import alarm.manoj.com.smartgpsalarm.models.GPSAlarm;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class AlarmWarningView extends RelativeLayout
+{
+
+    public AlarmWarningView(final Context context, GPSAlarm alarm)
+    {
+        super(context);
+        View alarmWarningView = LayoutInflater.from(context).inflate(R.layout.alarm_warning_overlay, null);
+        ((TextView)alarmWarningView.findViewById(R.id.alarm_warning_title)).setText(alarm.getTitle());
+        alarmWarningView.findViewById(R.id.alarm_warning_dismiss).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                AlarmRinger.getInstance(context).stopAlarm();
+            }
+        });
+        addView(alarmWarningView);
+    }
+}
