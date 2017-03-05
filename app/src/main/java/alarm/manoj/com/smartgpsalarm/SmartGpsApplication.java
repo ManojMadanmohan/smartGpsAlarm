5f7d48c4ff01dfc419687371f794f2541dbdd3a4
Manoj <manoj@zophop.com>
2017-03-06 00:02:56 +0530

    crashlytics integration added

--- app/src/main/java/alarm/manoj/com/smartgpsalarm/SmartGpsApplication.java
+++ app/src/main/java/alarm/manoj/com/smartgpsalarm/SmartGpsApplication.java
@@ -2,6 +2,8 @@ package alarm.manoj.com.smartgpsalarm;
 
 import alarm.manoj.com.smartgpsalarm.features.LocationFeature;
 import android.app.Application;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class SmartGpsApplication extends Application
 {
@@ -9,6 +11,7 @@ public class SmartGpsApplication extends Application
     public void onCreate()
     {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
         LocationFeature.getInstance(this);
     }
 }
