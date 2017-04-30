2bd64723cc7ee9181324fc0d95521f42d6b7a779
Manoj <manoj@zophop.com>
2017-05-01 01:11:53 +0530

    base presenter and view added for MVP migration

---
+++ app/src/main/java/alarm/manoj/com/smartgpsalarm/ui/base/BasePresenter.java
@@ -0,0 +1,16 @@
+package alarm.manoj.com.smartgpsalarm.ui.base;
+
+public interface BasePresenter
+{
+    public void onCreate();
+
+    public void onStart();
+
+    public void onResume();
+
+    public void onPause();
+
+    public void onStop();
+
+    public void onDestroy();
+}
---
+++ app/src/main/java/alarm/manoj/com/smartgpsalarm/ui/base/BaseView.java
@@ -0,0 +1,6 @@
+package alarm.manoj.com.smartgpsalarm.ui.base;
+
+public interface BaseView<T>
+{
+
+}
